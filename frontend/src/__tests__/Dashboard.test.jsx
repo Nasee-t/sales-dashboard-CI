c7eef21c3e89fcb9ad3d5789068e94b51d3585be
Naseeha <lunagood0123@gmail.com>
2026-06-01 11:29:50 +0530

    Fix broken test 2

--- frontend/src/__tests__/Dashboard.test.jsx
+++ frontend/src/__tests__/Dashboard.test.jsx
@@ -26,6 +26,6 @@ describe("Dashboard", () => {
 
     render(<Dashboard />);
 
-    expect(screen.getByText("Break this test")).toBeInTheDocument();
+    expect(screen.getByText("Sales Command Center")).toBeInTheDocument();
   });
 });
